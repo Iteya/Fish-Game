53dd3316e3e93eeb58c1f8460fb304bfc8d53045
dylanJasperX1 <djasper5983@student.pps.net>
2023-12-08 11:40:16 -0800

    Closer to working fish

--- Assets/Dylan/Scripts/FishMovement-LeftSide.cs
+++ Assets/Dylan/Scripts/FishMovement-LeftSide.cs
@@ -3,27 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class FishMovement : MonoBehaviour
 {
     public float speed = 2;
+    public float startPosition = Random.Range(4.34f, -4.39f);
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position += new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Trigger"))
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (transform.position.x < -9.6)
         {
-            UnityEditor.PrefabUtility.ResetToPrefabState(this.gameObject);
+            transform.position += new Vector3(20, 0, 0);
+            
         }
     }
+
+    
 }
--- Assets/Dylan/Scripts/LineMovement.cs
+++ Assets/Dylan/Scripts/LineMovement.cs
@@ -1,25 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class LineMovement : MonoBehaviour
-{ 
-    float movementSpeedY;
+{
+    // Start is called before the first frame update
+    float movementSpeed;
     float yDirection;
     float yVector;
-    // Start is called before the first frame update
     void Start()
     {
-        movementSpeedY = 5f;
+        movementSpeed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         yDirection = Input.GetAxis("Vertical");
-        yVector = yDirection * movementSpeedY * Time.deltaTime;
+        yVector = yDirection * movementSpeed * Time.deltaTime;
         transform.position = transform.position + new Vector3(0, yVector, 0);
-        
     }
 }
