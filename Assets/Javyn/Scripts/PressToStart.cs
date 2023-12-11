a566a4c5ced9eff3954e73f488097359fd8a4c11
Iteya <jandrico7658@student.pps.net>
2023-12-11 09:34:07 -0800

    Got scene switching to work

--- Assets/Javyn/Scripts/PressToStart.cs
+++ Assets/Javyn/Scripts/PressToStart.cs
@@ -6,13 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class PressToStart : MonoBehaviour
 {
-    public string TargetScene = "Playscene";
-
     void Update()
     {
         if (Input.GetAxisRaw("Buttons1") != 0 || Input.GetAxisRaw("Buttons2") != 0)
         {
-            //SceneManager.LoadScene(TargetScene); <-- Currently BROKEN (talk to owen to fix when you get the chance?
+            SceneManager.LoadScene("Playscene");
         }       
     }
 }
